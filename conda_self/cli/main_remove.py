2344b48ae8c2c953d9fca4cdaf618e608d726f98
sophia <scastellarin@quansight.com>
2025-06-23 09:20:33 -0700

    Raise InvalidSpec exception when trying to remove a spec that must not be removed

--- conda_self/cli/main_remove.py
+++ conda_self/cli/main_remove.py
@@ -15,14 +15,18 @@ def configure_parser(parser: argparse.ArgumentParser) -> None:
 
 
 def execute(args: argparse.Namespace) -> int:
+    from ..exceptions import SpecsCanNotBeRemoved
     from ..query import permanent_dependencies
     from ..install import uninstall_specs_in_protected_env
 
     uninstallable_packages = permanent_dependencies()
+    invalid_specs = []
     for spec in args.specs:
         if spec in uninstallable_packages:
-            print(f"Package '{spec}' can not be removed. Aborting!")
-            return 0
+            invalid_specs.append(spec)
+            
+    if invalid_specs:     
+        raise SpecsCanNotBeRemoved(invalid_specs)
 
     print("Removing plugins:", *args.specs)
 
--- conda_self/exceptions.py
+++ conda_self/exceptions.py
@@ -4,3 +4,8 @@ from conda.exceptions import CondaError
 class SpecsAreNotPlugins(CondaError):
     def __init__(self, specs: list[str]):
         super().__init__(f"The following requested specs are not plugins: {specs}")
+
+
+class SpecsCanNotBeRemoved(CondaError):
+    def __init__(self, specs: list[str]):
+        super().__init__(f"Packages '{specs}' can not be removed.")
